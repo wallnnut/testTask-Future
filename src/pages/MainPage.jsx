d1797067cf6f0730893d1b806c2b1e7ded0fe232
wallnnut <81454917+wallnnut@users.noreply.github.com>
2023-03-17 16:44:17 +0300

    init project

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const MainPage = () => {
+	return (
+		<>
+			<header>
+				<Container></Container>
+			</header>
+			<main></main>
+		</>
+	);
+};
+
+export default MainPage;
